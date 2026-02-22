fd55da4f020387db174b626b1add8918b10c5145
jeevikagowda7 <anjeevikagowda7@gmail.com>
2026-02-22 17:47:35 +0530

    Add files via upload
    Initial implementation of 2:1 MUX simulation

---
+++ Code2.m
@@ -0,0 +1,9 @@
+I0=input('enter the value for I0:');
+I1=input('enter the value for I1:');
+S=input('enter the value for select line:');
+if S==0
+    y=I0;
+else
+    y=I1;
+end
+disp(['output y is:' num2str(y)]);
